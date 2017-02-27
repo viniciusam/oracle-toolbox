65cbb0106eb42484d35e21b1c61acf4590b13a04
Vinicius Avellar <amvinicius@gmail.com>
2017-02-27 11:16:25 -0300

    Trigger travis build.

--- toolbox.sh
+++ toolbox.sh
@@ -1,18 +1,30 @@
 
 WORKDIR=$(pwd)
-echo "Cache Dir: $CACHE_DIR"
 
-if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip ]; then
-    sh download.sh -p se12c
-    mv linuxamd64_12102_database_se2_1of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip
-    mv linuxamd64_12102_database_se2_2of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip
+# if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip ]; then
+#     sh download.sh -p se12c
+#     mv linuxamd64_12102_database_se2_1of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip
+#     mv linuxamd64_12102_database_se2_2of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip
+# fi
+
+# cp $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
+# cp $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
+
+# cd dockerfiles
+# sh buildDockerImage.sh -s
+
+# cd $WORKDIR
+# sh run_se12.sh
+
+if [ ! -f $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip ]; then
+    sh download.sh -p xe11g
+    mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 fi
 
-cp $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
-cp $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
+cp $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip dockerfiles/11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 
 cd dockerfiles
-sh buildDockerImage.sh -s
+sh buildDockerImage.sh -v 11.2.0.2
 
 cd $WORKDIR
-sh run_se12.sh
+sh run_xe11.sh
