c6e0073d03ba32031ef0dfb058e69fc861bae946
Vinicius Avellar <amvinicius@gmail.com>
2017-02-27 10:13:28 -0300

    Caching downloaded files. Trigger travis build.

--- toolbox.sh
+++ toolbox.sh
@@ -1,9 +1,14 @@
 
 WORKDIR=$(pwd)
 
-sh download.sh -p se12c
-mv linuxamd64_12102_database_se2_1of2.zip ./dockerfiles/12.1.0.2
-mv linuxamd64_12102_database_se2_2of2.zip ./dockerfiles/12.1.0.2
+if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip ]; then
+    sh download.sh -p se12c
+    mv linuxamd64_12102_database_se2_1of2.zip $CACHE_DIR
+    mv linuxamd64_12102_database_se2_2of2.zip $CACHE_DIR
+fi
+
+ln -s $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
+ln -s $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
 
 cd dockerfiles
 sh buildDockerImage.sh -s
