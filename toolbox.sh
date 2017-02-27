a19d9c6abc9223b77d1695cc3a8d44ffd8cd2f98
Vinicius Avellar <amvinicius@gmail.com>
2017-02-27 14:17:52 -0300

    Verifying travis memory and disk space.

--- toolbox.sh
+++ toolbox.sh
@@ -1,16 +1,8 @@
 
 WORKDIR=$(pwd)
 
-npm install -g phantomjs-prebuilt casperjs
-sh download.sh -p se12c
-mv linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
-mv linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
-
-cd dockerfiles
-sh buildDockerImage.sh -s
-
-cd $WORKDIR
-sh run_se12.sh
+free -m
+df -h
 
 # if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip ]; then
 #     npm install -g phantomjs-prebuilt casperjs
