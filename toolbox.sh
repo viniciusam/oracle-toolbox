ac48f96100e597ba912c57e47bbc73179c4836f4
Vinicius Avellar <amvinicius@gmail.com>
2017-02-27 12:19:34 -0300

    Trigger travis build.

--- toolbox.sh
+++ toolbox.sh
@@ -1,6 +1,17 @@
 
 WORKDIR=$(pwd)
 
+npm install -g phantomjs-prebuilt casperjs
+sh download.sh -p se12c
+mv linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
+mv linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
+
+cd dockerfiles
+sh buildDockerImage.sh -s
+
+cd $WORKDIR
+sh run_se12.sh
+
 # if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip ]; then
 #     npm install -g phantomjs-prebuilt casperjs
 #     sh download.sh -p se12c
@@ -17,16 +28,16 @@ WORKDIR=$(pwd)
 # cd $WORKDIR
 # sh run_se12.sh
 
-if [ ! -f $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip ]; then
-    npm install -g phantomjs-prebuilt casperjs
-    sh download.sh -p xe11g
-    mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
-fi
+# if [ ! -f $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip ]; then
+#     npm install -g phantomjs-prebuilt casperjs
+#     sh download.sh -p xe11g
+#     mv oracle-xe-11.2.0-1.0.x86_64.rpm.zip $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+# fi
 
-cp $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip dockerfiles/11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+# cp $CACHE_DIR/oracle-xe-11.2.0-1.0.x86_64.rpm.zip dockerfiles/11.2.0.2/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
 
-cd dockerfiles
-sh buildDockerImage.sh -v 11.2.0.2
+# cd dockerfiles
+# sh buildDockerImage.sh -v 11.2.0.2
 
-cd $WORKDIR
-sh run_xe11.sh
+# cd $WORKDIR
+# sh run_xe11.sh
