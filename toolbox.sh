cfab098a168d710ad284fc5fdaf9ed9a3d27f231
Vinicius Avellar <amvinicius@gmail.com>
2017-02-27 10:20:41 -0300

    Trigger travis build.

--- toolbox.sh
+++ toolbox.sh
@@ -1,14 +1,15 @@
 
 WORKDIR=$(pwd)
+echo "Cache Dir: $CACHE_DIR"
 
-if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip ]; then
+if [ ! -f $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip ]; then
     sh download.sh -p se12c
-    mv linuxamd64_12102_database_se2_1of2.zip $CACHE_DIR
-    mv linuxamd64_12102_database_se2_2of2.zip $CACHE_DIR
+    mv linuxamd64_12102_database_se2_1of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip
+    mv linuxamd64_12102_database_se2_2of2.zip $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip
 fi
 
-ln -s $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
-ln -s $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
+cp $CACHE_DIR/linuxamd64_12102_database_se2_1of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_1of2.zip
+cp $CACHE_DIR/linuxamd64_12102_database_se2_2of2.zip dockerfiles/12.1.0.2/linuxamd64_12102_database_se2_2of2.zip
 
 cd dockerfiles
 sh buildDockerImage.sh -s
