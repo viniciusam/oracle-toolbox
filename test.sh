06e0c3e61d01e26e50b18d808a4bd44c186dbe7c
Vinicius Avellar <amvinicius@gmail.com>
2017-03-20 21:12:16 -0300

    Changing download dir.

--- test.sh
+++ test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-mkdir /downloads
+DOWNLOADS_DIR=$HOME/downloads
+mkdir $DOWNLOADS_DIR
 
 docker run --rm \
 	-e ORACLE_OTN_USER="$ORACLE_OTN_USER" \
 	-e ORACLE_OTN_PASSWORD="$ORACLE_OTN_PASSWORD" \
-	-v /downloads:/downloads \
+	-v $DOWNLOADS_DIR:/downloads \
 	viniciusam/oracle-toolbox -p sqlcl
 
-if [ ! -f /downloads/sqlcl*.zip ]; then
-    echo "Error: download file not found..."
+if [ ! -f $DOWNLOADS_DIR/sqlcl*.zip ]; then
+    echo "Error: downloaded file not found..."
     exit 1
 fi
 
