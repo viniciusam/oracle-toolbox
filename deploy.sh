e8e3af4169601e8c99ffd3fe9e76aeaf356fd946
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-03-29 07:25:09 -0300

    Deployment to master branch only.

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ ! $TRAVIS_BRANCH == "master" ] || [ $TRAVIS_PULL_REQUEST ]; then
+    echo "Skipping push step."
+    exit 0;
+fi
+
+echo "Pushing images to Docker Hub."
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+docker push viniciusam/oracle-toolbox
